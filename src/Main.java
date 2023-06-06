13048eb876e71b92beb01815b75348ba0a44eefe
Evan Thomas <ethomas0512@gmail.com>
2023-06-06 14:37:33 -0700

    Fixing linter errors

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+/**
+ * An example program
+ *
+ * @author Evan Thomas
+ * @version 1.0
+ */
 public class Main {
+    /**
+     *  Entry point
+     * @param args command line args
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
